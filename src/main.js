2cb3789f4e0cada68aa37b7cc8b7a32bb78b34a3
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2024-08-09 15:19:33 +0800

    123

---
+++ src/main.js
@@ -0,0 +1,6 @@
+// 运行在 Electron 主进程 下的插件入口
+
+// 创建窗口时触发
+module.exports.onBrowserWindowCreated = window => {
+    // window 为 Electron 的 BrowserWindow 实例
+}
---
+++ src/renderer.js
@@ -0,0 +1,6 @@
+// 运行在 Electron 渲染进程 下的页面脚本
+
+// 打开设置界面时触发
+export const onSettingWindowCreated = view => {
+    // view 为 Element 对象，修改将同步到插件设置界面
+}
