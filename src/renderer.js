3629107d1a32653ede66f8db27c22aaa81af2b78
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2024-08-10 14:45:31 +0800

    无敌了
    为什么renderer里面import随便一个函数，然后NTQQ的设置界面的全部插件设置栏都不见了？？？

--- src/renderer.js
+++ src/renderer.js
@@ -1,6 +1,15 @@
+import {test123} from "./MenuUtils";
 // 运行在 Electron 渲染进程 下的页面脚本
-
 // 打开设置界面时触发
 export const onSettingWindowCreated = view => {
     // view 为 Element 对象，修改将同步到插件设置界面
+    // 这个函数导出之后在QQ设置里面可以直接看见插件页面
+     // 创建一个新的 div 元素
+    const div = document.createElement('div');
+    div.textContent = '嘻嘻哈哈，哇嘎哇嘎';
+
+    // 将 div 元素添加到 view 对象中
+    view.appendChild(div);
 }
+
+test123()
