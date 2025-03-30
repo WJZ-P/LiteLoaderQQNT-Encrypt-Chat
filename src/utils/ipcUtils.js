cd18ec04b84385d9b2c62f1a5e25c2a92f27638e
WJZ_P <110795301+WJZ-P@users.noreply.github.com>
2025-03-30 18:59:16 +0800

    233

--- src/utils/ipcUtils.js
+++ src/utils/ipcUtils.js
@@ -25,7 +25,7 @@ function ipcModifyer(ipcProxy, window) {
                 const eventName = args?.[3]?.[0]?.eventName
                 //测试
                 //if(ipcName==='nodeIKernelMsgService/ForwardMsgWithComment') console.log(JSON.stringify(args))
-                if (eventName !== "ns-LoggerApi-2") console.log(JSON.stringify(args))//调试的时候用
+                //if (eventName !== "ns-LoggerApi-2") console.log(JSON.stringify(args))//调试的时候用
 
                 if (ipcName === 'nodeIKernelMsgService/sendMsg') modifiedArgs = await ipcMsgModify(args, window);
                 if (ipcName === 'openMediaViewer') modifiedArgs = ipcOpenImgModify(args);
